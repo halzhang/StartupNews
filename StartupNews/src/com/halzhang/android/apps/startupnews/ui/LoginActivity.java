e902371bb80c29d782a490e46eea6846aa1f5dc9
halzhang <zhg_grj@163.com>
2013-07-02 22:24:26 +0800

    优化登录界面布局，在tablet好看点

--- StartupNews/src/com/halzhang/android/apps/startupnews/ui/LoginActivity.java
+++ StartupNews/src/com/halzhang/android/apps/startupnews/ui/LoginActivity.java
@@ -7,6 +7,7 @@
 package com.halzhang.android.apps.startupnews.ui;
 
 import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragment;
 import com.actionbarsherlock.view.MenuItem;
 import com.google.analytics.tracking.android.EasyTracker;
 import com.halzhang.android.apps.startupnews.Constants.IntentAction;
@@ -31,7 +32,9 @@ import org.jsoup.select.Elements;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -39,7 +42,9 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.webkit.CookieManager;
 import android.webkit.CookieSyncManager;
@@ -49,6 +54,7 @@ import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,31 +70,17 @@ import java.util.List;
 public class LoginActivity extends BaseFragmentActivity {
 
     private static final String LOG_TAG = LoginActivity.class.getSimpleName();
-
+    
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
 
-    // Values for email and password at the time of the login attempt.
-    private String mUsername;
-
-    private String mPassword;
-
-    // UI references.
-    private EditText mUsernameView;
-
-    private EditText mPasswordView;
-
-    private View mLoginFormView;
-
-    private View mLoginStatusView;
-
-    private TextView mLoginStatusMessageView;
-
     private String mFnid;
 
     private LoginPreTask mPreTask;
+    
+    private LoginFragment mLoginFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,26 +88,9 @@ public class LoginActivity extends BaseFragmentActivity {
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_login);
-
-        mUsernameView = (EditText) findViewById(R.id.username);
-        mPasswordView = (EditText) findViewById(R.id.password);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        mLoginFormView = findViewById(R.id.login_form);
-        mLoginStatusView = findViewById(R.id.login_status);
-        mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
-        mLoginStatusMessageView.setText(R.string.login_progress_init);
         SessionManager.getInstance(this).clear();
-        showProgress(true);
+        mLoginFragment = new LoginFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mLoginFragment).commit();
         mPreTask = new LoginPreTask();
         mPreTask.execute("");
     }
@@ -135,7 +110,7 @@ public class LoginActivity extends BaseFragmentActivity {
             case R.id.menu_login:
                 EasyTracker.getTracker().sendEvent("ui_action", "options_item_selected",
                         "loginactivity_menu_login", 0L);
-                attemptLogin();
+                mLoginFragment.attemptLogin();
                 return true;
             default:
                 break;
@@ -143,82 +118,13 @@ public class LoginActivity extends BaseFragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
-     */
-    public void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
-        // Reset errors.
-        mUsernameView.setError(null);
-        mPasswordView.setError(null);
-
-        mUsername = mUsernameView.getText().toString();
-        mPassword = mPasswordView.getText().toString();
-
-        boolean cancel = false;
-        View focusView = null;
-
-        if (TextUtils.isEmpty(mPassword)) {
-            mPasswordView.setError(getString(R.string.error_field_required));
-            focusView = mPasswordView;
-            cancel = true;
-        }
-        if (TextUtils.isEmpty(mUsername)) {
-            mUsernameView.setError(getString(R.string.error_field_required));
-            focusView = mUsernameView;
-            cancel = true;
-        }
-
-        if (cancel) {
-            focusView.requestFocus();
-        } else {
-            mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
-            showProgress(true);
-            mAuthTask = new UserLoginTask();
-            mAuthTask.execute((Void) null);
-        }
-    }
-
-    /**
-     * Shows the progress UI and hides the login form.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginStatusView.setVisibility(View.INVISIBLE);
-            mLoginStatusView.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0)
-                    .setListener(new AnimatorListenerAdapter() {
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
-                        }
-                    });
-
-            mLoginFormView.setVisibility(View.INVISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1)
-                    .setListener(new AnimatorListenerAdapter() {
-                        @Override
-                        public void onAnimationEnd(Animator animation) {
-                            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                        }
-                    });
-        } else {
-            mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }
+  
 
     /**
      * Represents an asynchronous login/registration task used to authenticate
      * the user.
      */
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     public class UserLoginTask extends AsyncTask<Void, Void, String> {
         @Override
         protected String doInBackground(Void... params) {
@@ -235,8 +141,8 @@ public class LoginActivity extends BaseFragmentActivity {
             httpPost.addHeader("Connection", "keep-alive");
             ArrayList<NameValuePair> valuePairs = new ArrayList<NameValuePair>(3);
             valuePairs.add(new BasicNameValuePair("fnid", mFnid));
-            valuePairs.add(new BasicNameValuePair("p", mPassword));
-            valuePairs.add(new BasicNameValuePair("u", mUsername));
+            valuePairs.add(new BasicNameValuePair("p", mLoginFragment.mPassword));
+            valuePairs.add(new BasicNameValuePair("u", mLoginFragment.mUsername));
             try {
                 UrlEncodedFormEntity entity = new UrlEncodedFormEntity(valuePairs, HTTP.UTF_8);
                 httpPost.setEntity(entity);
@@ -262,7 +168,7 @@ public class LoginActivity extends BaseFragmentActivity {
                         String value = cookie.getValue();
                         CDLog.i(LOG_TAG, "Cookie name: user " + " Value: " + cookie.getValue());
                         SessionManager.getInstance(getApplicationContext()).storeSesson(value,
-                                mUsername);
+                                mLoginFragment.mUsername);
                         user = value;
                     }
                     // sync cookie to webview
@@ -284,7 +190,7 @@ public class LoginActivity extends BaseFragmentActivity {
         @Override
         protected void onPostExecute(final String user) {
             mAuthTask = null;
-            showProgress(false);
+            mLoginFragment.showProgress(false);
             if (!TextUtils.isEmpty(user)) {
                 CDToast.showToast(getApplicationContext(), R.string.tip_login_success);
                 Intent intent = new Intent(IntentAction.ACTION_LOGIN);
@@ -299,7 +205,7 @@ public class LoginActivity extends BaseFragmentActivity {
         @Override
         protected void onCancelled() {
             mAuthTask = null;
-            showProgress(false);
+            mLoginFragment.showProgress(false);
         }
     }
 
@@ -350,15 +256,147 @@ public class LoginActivity extends BaseFragmentActivity {
             super.onPostExecute(result);
             mFnid = result;
             mPreTask = null;
-            showProgress(false);
-            mUsernameView.requestFocus();
+            mLoginFragment.showProgress(false);
+            mLoginFragment.mUsernameView.requestFocus();
         }
 
         @Override
         protected void onCancelled() {
             super.onCancelled();
             mPreTask = null;
-            showProgress(false);
+            mLoginFragment.showProgress(false);
+        }
+
+    }
+    
+    @SuppressLint("ValidFragment")
+    private static class LoginFragment extends SherlockFragment{
+        
+        // Values for email and password at the time of the login attempt.
+        private String mUsername;
+
+        private String mPassword;
+
+        // UI references.
+        private EditText mUsernameView;
+
+        private EditText mPasswordView;
+
+        private View mLoginFormView;
+
+        private View mLoginStatusView;
+
+        private TextView mLoginStatusMessageView;
+        
+        private WeakReference<LoginActivity> mLoginActivityRef;
+        
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+        
+        @Override
+        public void onAttach(Activity activity) {
+            super.onAttach(activity);
+            mLoginActivityRef = new WeakReference<LoginActivity>((LoginActivity)activity);
+        }
+        
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_login, null);
+            mUsernameView = (EditText) view.findViewById(R.id.username);
+            mPasswordView = (EditText) view.findViewById(R.id.password);
+            mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                    if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                        attemptLogin();
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+            mLoginFormView = view.findViewById(R.id.login_form);
+            mLoginStatusView = view.findViewById(R.id.login_status);
+            mLoginStatusMessageView = (TextView) view.findViewById(R.id.login_status_message);
+            mLoginStatusMessageView.setText(R.string.login_progress_init);
+            showProgress(true);
+            return view;
+        }
+        
+        /**
+         * Attempts to sign in or register the account specified by the login form.
+         * If there are form errors (invalid email, missing fields, etc.), the
+         * errors are presented and no actual login attempt is made.
+         */
+        public void attemptLogin() {
+            if (mLoginActivityRef.get().mAuthTask != null) {
+                return;
+            }
+
+            // Reset errors.
+            mUsernameView.setError(null);
+            mPasswordView.setError(null);
+
+            mUsername = mUsernameView.getText().toString();
+            mPassword = mPasswordView.getText().toString();
+
+            boolean cancel = false;
+            View focusView = null;
+
+            if (TextUtils.isEmpty(mPassword)) {
+                mPasswordView.setError(getString(R.string.error_field_required));
+                focusView = mPasswordView;
+                cancel = true;
+            }
+            if (TextUtils.isEmpty(mUsername)) {
+                mUsernameView.setError(getString(R.string.error_field_required));
+                focusView = mUsernameView;
+                cancel = true;
+            }
+
+            if (cancel) {
+                focusView.requestFocus();
+            } else {
+                mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+                showProgress(true);
+                mLoginActivityRef.get().mAuthTask = mLoginActivityRef.get().new UserLoginTask();
+                mLoginActivityRef.get().mAuthTask.execute((Void) null);
+            }
+        }
+        
+
+        /**
+         * Shows the progress UI and hides the login form.
+         */
+        @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+        public void showProgress(final boolean show) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+                int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+                mLoginStatusView.setVisibility(View.INVISIBLE);
+                mLoginStatusView.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+                            }
+                        });
+
+                mLoginFormView.setVisibility(View.INVISIBLE);
+                mLoginFormView.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1)
+                        .setListener(new AnimatorListenerAdapter() {
+                            @Override
+                            public void onAnimationEnd(Animator animation) {
+                                mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                            }
+                        });
+            } else {
+                mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+                mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            }
         }
 
     }
