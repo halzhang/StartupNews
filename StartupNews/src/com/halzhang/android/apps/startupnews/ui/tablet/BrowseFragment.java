6ee3024853e9c90d294a2de50373598ab4d01161
halzhang <zhg_grj@163.com>
2013-07-10 22:33:10 +0800

    优化初始化

--- StartupNews/src/com/halzhang/android/apps/startupnews/ui/tablet/BrowseFragment.java
+++ StartupNews/src/com/halzhang/android/apps/startupnews/ui/tablet/BrowseFragment.java
@@ -1,36 +1,49 @@
+
 package com.halzhang.android.apps.startupnews.ui.tablet;
 
+import com.actionbarsherlock.app.SherlockFragment;
 import com.halzhang.android.apps.startupnews.R;
 import com.halzhang.android.apps.startupnews.ui.widgets.WebViewController;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 
 /**
- * 浏览器
- * Created by Hal on 13-5-25.
+ * 浏览器 Created by Hal on 13-5-25.
  */
-public class BrowseFragment extends Fragment {
+public class BrowseFragment extends SherlockFragment {
 
     private WebView mWebView;
-    
+
     private WebViewController mWebViewController;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mWebViewController = new WebViewController(getActivity());
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         final View view = inflater.inflate(R.layout.fragment_article, null);
         mWebView = (WebView) view.findViewById(R.id.webview);
-        mWebViewController = new WebViewController(getActivity());
         mWebViewController.initControllerView(mWebView, view);
         return view;
     }
 
     public void load(String url) {
         mWebViewController.loadUrl(url);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu,
+            com.actionbarsherlock.view.MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_news_option, menu);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
 }
