a38fe2c5d0720ae6a7b392d17cf6e6a975618a12
halzhang <zhg_grj@163.com>
2015-02-07 18:23:06 +0800

    支持productFlavors 构建

---
+++ StartupNewsApp/src/main/java/com/halzhang/android/apps/startupnews/ui/LauncherActivity.java
@@ -0,0 +1,7 @@
+package com.halzhang.android.apps.startupnews.ui;
+
+/**
+ * Created by Hal on 15/2/7.
+ */
+public class LauncherActivity extends BaseFragmentActivity {
+}
---
+++ StartupNewsApp/src/tv/java/com/halzhang/android/apps/startupnews/TVApp.java
@@ -0,0 +1,14 @@
+package com.halzhang.android.apps.startupnews;
+
+import android.app.Application;
+
+/**
+ * Created by Hal on 15/1/31.
+ */
+public class TVApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
