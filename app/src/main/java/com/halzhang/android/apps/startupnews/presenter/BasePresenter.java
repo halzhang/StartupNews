2b66e73dc3eda6b3a50be7a61a1a3b4bde345d9f
halzhang <zhg_grj@163.com>
2015-05-06 11:32:19 +0800

    update

---
+++ app/src/main/java/com/halzhang/android/apps/startupnews/presenter/BasePresenter.java
@@ -0,0 +1,7 @@
+package com.halzhang.android.apps.startupnews.presenter;
+
+/**
+ * Created by Hal on 15/4/26.
+ */
+public class BasePresenter {
+}
---
+++ app/src/main/java/com/halzhang/android/apps/startupnews/presenter/LoginPresenter.java
@@ -0,0 +1,12 @@
+package com.halzhang.android.apps.startupnews.presenter;
+
+import com.halzhang.android.apps.startupnews.ui.LoginActivity;
+import com.halzhang.android.mvp.presenter.Presenter;
+
+/**
+ * 登录
+ * Created by Hal on 15/5/5.
+ */
+public class LoginPresenter extends Presenter<LoginActivity> {
+
+}
