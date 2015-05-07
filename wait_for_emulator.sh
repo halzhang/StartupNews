9852803ad7c827eb483fec209bcecc81f5af4f0b
halzhang <zhg_grj@163.com>
2015-05-07 09:44:44 +0800

    update ci

--- wait_for_emulator.sh
+++ wait_for_emulator.sh
@@ -1,8 +1,21 @@
-until [ "`adb devices | grep "emulator-[0-9][0-9]*[[:space:]]*device"`" ]
-do
-  echo "waiting for emulator..."
-  sleep 10
+#!/bin/bash
+
+# Originally written by Ralf Kistner <ralf@embarkmobile.com>, but placed in the public domain
+
+set +e
+
+bootanim=""
+failcounter=0
+until [[ "$bootanim" =~ "stopped" ]]; do
+   bootanim=`adb -e shell getprop init.svc.bootanim 2>&1`
+   echo "$bootanim"
+   if [[ "$bootanim" =~ "not found" ]]; then
+      let "failcounter += 1"
+      if [[ $failcounter -gt 15 ]]; then
+        echo "Failed to start emulator"
+        exit 1
+      fi
+   fi
+   sleep 1
 done
-echo "emulator ready.  waiting a sec because emulators are fickle beasts..."
-sleep 45
-echo "Ready to roll"
+echo "Done"
