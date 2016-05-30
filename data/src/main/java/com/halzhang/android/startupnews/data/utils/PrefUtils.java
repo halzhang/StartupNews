63e2aae2f22efef2d3f62271f54543dcff5fe27e
zhanghanguo <ghanguo@gmail.com>
2016-05-30 18:23:08 +0800

    add prefutils

---
+++ data/src/main/java/com/halzhang/android/startupnews/data/utils/PrefUtils.java
@@ -0,0 +1,18 @@
+package com.halzhang.android.startupnews.data.utils;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by zhanghanguo@yy.com on 2016/5/30.
+ */
+public class PrefUtils {
+
+    public static void set(Context ctx, final String key, final String value) {
+        PreferenceManager.getDefaultSharedPreferences(ctx).edit().putString(key, value).apply();
+    }
+
+    public static String get(Context context, final String key) {
+        return PreferenceManager.getDefaultSharedPreferences(context).getString(key, null);
+    }
+}
